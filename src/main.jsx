56a399d67a630a17f76fe7d0d14a05eaf5af315d
Jean Alves <jeanjalves0@gmail.com>
2026-05-19 17:49:18 -0300

    Corrigindo inicializacao do PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,6 @@ import App from './App.jsx'
 
 import './styles/global.css'
 
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({
-  immediate: true
-})
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
